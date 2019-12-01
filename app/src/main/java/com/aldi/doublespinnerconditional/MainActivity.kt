8c51493e672f3f8e40865daca63c3f686d778317
ganiaaldi <mrgad98@gmail.com>
2019-12-01 14:00:16 +0700

    upgrade ui/ux & add more function code

--- app/src/main/java/com/aldi/doublespinnerconditional/MainActivity.kt
+++ app/src/main/java/com/aldi/doublespinnerconditional/MainActivity.kt
@@ -9,65 +9,103 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.widget.ToolbarWidgetWrapper
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var category1 : String? = null
-    var category2 : String? = null
+    var category1: String? = null
+    var category2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         chooseCategorySpinner1()
-        chooseCategorySpinner2()
+
         showText()
     }
 
 
     private fun chooseCategorySpinner1() {
-        val categoryArray = ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item, resources.getStringArray(R.array.category_primary))
-        spinner1.adapter = categoryArray
-        spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-
-            }
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-               category1 = categoryArray?.getItem(p2).toString()
-                when (p2){
-                    0 ->
-                    {
-                        val category2Array  = ArrayAdapter(this@MainActivity,android.R.layout.simple_spinner_dropdown_item, resources.getStringArray(R.array.category_flowers))
-                        spinner2.adapter = category2Array
+        layout1.setOnClickListener {
+            if (layout1.isVisible) {
+                layout1.visibility = View.INVISIBLE
+                val categoryArray = ArrayAdapter(
+                    this,
+                    android.R.layout.simple_spinner_dropdown_item,
+                    resources.getStringArray(R.array.category_primary)
+                )
+                spinner1.adapter = categoryArray
+                spinner1.performClick()
+                chooseCategorySpinner2()
+                spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onNothingSelected(p0: AdapterView<*>?) {
 
                     }
-                    1 ->
-                    {
-                        val category2Array  = ArrayAdapter(this@MainActivity,android.R.layout.simple_spinner_dropdown_item, resources.getStringArray(R.array.category_fruits))
-                        spinner2.adapter = category2Array
-                    }   
-                    2 ->
-                    {
-                        val category2Array  = ArrayAdapter(this@MainActivity,android.R.layout.simple_spinner_dropdown_item, resources.getStringArray(R.array.category_vegetables))
-                        spinner2.adapter = category2Array
-                    }    
+
+                    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                        category1 = categoryArray?.getItem(p2).toString()
+                        when (p2) {
+                            0 -> {
+                                val category2Array = ArrayAdapter(
+                                    this@MainActivity,
+                                    android.R.layout.simple_spinner_dropdown_item,
+                                    resources.getStringArray(R.array.category_flowers)
+                                )
+                                spinner2.adapter = category2Array
+
+                            }
+                            1 -> {
+                                val category2Array = ArrayAdapter(
+                                    this@MainActivity,
+                                    android.R.layout.simple_spinner_dropdown_item,
+                                    resources.getStringArray(R.array.category_fruits)
+                                )
+                                spinner2.adapter = category2Array
+                            }
+                            2 -> {
+                                val category2Array = ArrayAdapter(
+                                    this@MainActivity,
+                                    android.R.layout.simple_spinner_dropdown_item,
+                                    resources.getStringArray(R.array.category_vegetables)
+                                )
+                                spinner2.adapter = category2Array
+                            }
+                        }
+                    }
                 }
+                spinner1.visibility = View.VISIBLE
+            } else {
+                layout1.visibility = View.VISIBLE
             }
         }
     }
+
     private fun chooseCategorySpinner2() {
-        spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-            }
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                category2 = spinner2.getItemAtPosition(p2).toString()
+        layout2.visibility = View.VISIBLE
+        layout2.setOnClickListener {
+            if (layout2.isVisible) {
+                layout2.visibility = View.INVISIBLE
+                spinner2.performClick()
+                spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onNothingSelected(p0: AdapterView<*>?) {
+                    }
+
+                    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                        category2 = spinner2.getItemAtPosition(p2).toString()
+                    }
+                }
             }
         }
     }
 
     private fun showText() {
         btn.setOnClickListener {
-           tv1.text = category1.toString()
-            tv2.text = category2.toString()
+            if (category1 == null && category2 == null) {
+                Toast.makeText(this,"Mohon Pilih Terlebih Dahulu!",Toast.LENGTH_LONG)
+            } else {
+                tv1.text = category1.toString()
+                tv2.text = category2.toString()
+            }
         }
     }
 }
